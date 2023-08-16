ead6739b43d85f9884c28fa848591f2403f0bccd
Sagar Prasad <sagar.prasad2299@gmail.com>
2023-08-16 22:00:45 +0530

    error handling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar/navbar";
 import Intro from "./components/Intro/intro";
-import Skills from "./components/about/skills";
+import Skills from "./components/About/skills";
 import Works from "./components/works/works";
 
 const App = () => {
