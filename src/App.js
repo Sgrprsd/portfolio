56cd1ccbc84212140fca3cb9aa0f3e133300dba6
Sagar Prasad <sagar.prasad2299@gmail.com>
2023-08-16 21:57:10 +0530

    changes fixed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import Navbar from "./components/Navbar/navbar";
 import Intro from "./components/Intro/intro";
-import Skills from "./components/about/skills";
 import Works from "./components/works/works";
 import Contact from "./components/contact/contact";
+import Skills from "./components/About/skills";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Intro />
-      <Skills />
-      <Works />
+      <Skills/>
+      <Works/>
       <Contact />
     </div>
   );
