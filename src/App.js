147b92dd28ccaeef45b67cc3d051fa2aa976833c
Sagar Prasad <sagar.prasad2299@gmail.com>
2023-08-16 15:24:10 +0530

    header component added to the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar/navbar";
+import Intro from "./components/Intro/intro";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Intro/>
     </div>
   );
 };
