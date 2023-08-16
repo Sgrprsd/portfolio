7a1982eaea671a0aaf0ebf8ff38b7daa53e5589f
Sagar Prasad <sagar.prasad2299@gmail.com>
2023-08-16 23:57:13 +0530

    intro added with details
    intro added with lorem description

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./component/Navbar/navbar";
 import Footer from "./component/Footer/footer";
+import Intro from "./component/Intro/intro";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Footer/>
+      <Intro />
+      <Footer />
     </div>
   );
 };
