5b24a7b8ffad0b493789757b9408492534c88034
Sagar Prasad <sagar.prasad2299@gmail.com>
2023-08-16 16:41:10 +0530

    about section added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar/navbar";
 import Intro from "./components/Intro/intro";
+import Skills from "./components/about/skills";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Intro/>
+      <Intro />
+      <Skills />
     </div>
   );
 };
