2f064887e4f3623ca5e043f9132468ce759081c5
Sagar Prasad <sagar.prasad2299@gmail.com>
2023-08-16 23:49:46 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./component/Navbar/navbar";
+import Footer from "./component/Footer/footer";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Footer/>
     </div>
   );
 };
