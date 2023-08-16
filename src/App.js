1aa59fcd22e85134ea8f0616bee4a470b2ab89cd
Sagar Prasad <sagar.prasad2299@gmail.com>
2023-08-16 21:26:01 +0530

    updates folder names using Camel Casing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Navbar from "./components/Navbar/navbar";
 import Intro from "./components/Intro/intro";
-import Skills from "./components/about/skills";
-import Works from "./components/works/works";
-import Contact from "./components/contact/contact";
+import Skills from "./components/About/skills";
+import Works from "./components/Works/works";
+import Contact from "./components/Contact/contact";
 
 const App = () => {
   return (
